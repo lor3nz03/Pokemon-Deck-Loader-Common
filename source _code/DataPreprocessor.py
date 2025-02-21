2472b2e4c84e85df2d54ed770162ce14b07bd846
Lor3nz03 <lorenzodibella15@gmail.com>
2025-02-21 16:47:48 +0100

    Bug Fix and Clean

--- source _code/DataPreprocessor.py
+++ source _code/DataPreprocessor.py
@@ -13,10 +13,10 @@ class DataPreprocessor:
         self.data = pd.read_csv(self.file_path)
 
     def build_evolution_hierarchy(self):
-        # Create a dictionary to store the evolution hierarchy
+
         evolution_dict = {}
 
-        # Populate the dictionary with evolvesTo relationships
+        # Riempio il dizionario con le relazioni dei pokemon con la colonna evolveTo
         for _, row in self.data.iterrows():
             pokemon_name = row['name']
             evolves_to = ast.literal_eval(row['evolvesTo']) if not pd.isna(row['evolvesTo']) else []
@@ -26,7 +26,7 @@ class DataPreprocessor:
                     evolution_dict[pokemon_name] = []
                 evolution_dict[pokemon_name].extend(evolves_to)
 
-        # Create the evolveTo2 column for Basic Pokémon
+        # Creo una collona ausiliaria per creare successivamente la catena evolutiva dei pokemon
         def get_full_evolution_chain(pokemon_name, visited=None):
             if visited is None:
                 visited = set()
@@ -43,7 +43,7 @@ class DataPreprocessor:
             lambda row: ", ".join(get_full_evolution_chain(row['name'])[1:]) if 'Basic' in row['subtypes'] else '', axis=1
         )
 
-        # Create the gerarchic column based on the evolution hierarchy
+        # Creazione della colonna per le evoluzioni dei pokemon
         def get_evolution_chain(pokemon_name, visited=None):
             if visited is None:
                 visited = set()
@@ -61,16 +61,15 @@ class DataPreprocessor:
         )
 
     def clean_data(self):
-        # Filtrare solo i Pokémon
+        # Filtro solo i Pokémon
         self.data = self.data[self.data['supertype'] == 'Pokémon']
 
-        # Rimuovere i Pokémon con rarità diversa da "Rare Holo", "Rare", "Uncommon", "Common"
+        # Rimuovo i Pokémon con rarità diversa da "Rare Holo", "Rare", "Uncommon", "Common"
         allowed_rarities = ['Rare Holo', 'Rare', 'Uncommon', 'Common']
         self.data = self.data[self.data['rarity'].isin(allowed_rarities)]
 
-        # Rimuovere i Pokémon che non sono di subtypes "Basic", "Stage1", "Stage2"
+        # Rimuovo i Pokémon che non sono di subtypes "Basic", "Stage 1", "Stage 2"
         allowed_subtypes = ['Basic', 'Stage 1', 'Stage 2']
-
         def filter_subtypes(subtypes):
             try:
                 subtypes_list = ast.literal_eval(subtypes)
@@ -80,15 +79,16 @@ class DataPreprocessor:
                 pass
             return False
 
+        # Rimozione colonne inutile per le task che dobbiamo fare 
         self.data = self.data[self.data['subtypes'].apply(filter_subtypes)]
-
         cols_to_remove = ['set', 'series', 'publisher', 'release_date', 'artist', 'set_num', 'level', 
                           'abilities', 'retreatCost', 'convertedRetreatCost', 'flavorText', 
                           'nationalPokedexNumbers', 'legalities', 'resistances', 'rules', 
                           'regulationMark', 'ancientTrait','weaknesses','evolvesFrom']
         self.data.drop(columns=cols_to_remove, inplace=True, errors='ignore')
         
-        # Estrarre "attack_name" e "AVG_damage" dalla colonna "attacks"
+        # Estrazione delle feature "attack_name" e "AVG_damage" dalla colonna "attacks"
+        #Utilizzo libreria ast
         if 'attacks' in self.data.columns:
             def extract_attack_data(attacks):
                 if isinstance(attacks, str):
@@ -105,46 +105,44 @@ class DataPreprocessor:
 
             self.data[['attack_name', 'AVG_damage']] = self.data['attacks'].apply(lambda x: pd.Series(extract_attack_data(x)))
 
-        # Rimuovere la colonna originale "attacks"
+        # Rimuovo la colonna originale "attacks"
         self.data.drop(columns=['attacks'], inplace=True, errors='ignore')
 
-        # Riempimento valori nulli
+        # Se ci sono valori nulli ho deciso di non eliminarli 
         self.data.fillna({'hp': 0, 'types': 'Unknown', 'rarity': 'Common', 'evolvesFrom': '', 'AVG_damage': 0}, inplace=True)
         self.data['hp'] = pd.to_numeric(self.data['hp'], errors='coerce').fillna(0)
-        # Assicurarsi che i valori di AVG_damage non siano zero
+        # I valori di AVG_damage non devono essere zero
         self.data['AVG_damage'] = self.data['AVG_damage'].replace(0, self.data['AVG_damage'].mean())
-        # Arrotondare i valori di AVG_damage a numeri interi
+        # Arrotondo i valori di AVG_damage a numeri interi
         self.data['AVG_damage'] = self.data['AVG_damage'].round().astype(int)
-        # Build the evolution hierarchy and create the gerarchic column
+        # Richiamo il metodo per creare la colonna delle evoluzioni
         self.build_evolution_hierarchy()
+        # Cancello la colonna di supporto
         self.data.drop(columns='evolveTo2', inplace=True, errors='ignore')
 
+    # Salvo tutte queste modifiche in un file CSV a parte per comodità di visualizzazione
     def save_data(self, output_file_name):
-        # Trova la cartella "Project" salendo da "source_code"
+    
         base_dir = Path(__file__).resolve().parent.parent  # Sale di un livello
-
-        # Percorso corretto della cartella CSV
         output_dir = base_dir / "CSV"
-
-        # Percorso completo del file
         output_file_path = output_dir / output_file_name
-
         # Assicura che la cartella "CSV" esista
         output_dir.mkdir(parents=True, exist_ok=True)
 
-        # Sort the DataFrame by the 'subtypes' column
+        # Ordinamento dei dati per la visualizzazione utilizzati in fase di sviluppo
         #self.data = self.data.sort_values(by='subtypes')
         #self.data = self.data.sort_values(by='generation')
 
-        # Salva il file CSV
+        # Salvo il file CSV
         self.data.to_csv(output_file_path, index=False, encoding="utf-8")
 
-    def print_rarity_types(self):
-        # Extract unique rarity types
-        rarity_types = self.data['rarity'].unique()
-        print("Unique Rarity Types:")
-        for rarity in rarity_types:
-            print(rarity)
+    # Mi è servita per scoprire quanti tipi di rarità esistono nel database 
+
+    #def print_rarity_types(self):
+        #rarity_types = self.data['rarity'].unique()
+        #print("Unique Rarity Types:")
+        #for rarity in rarity_types:
+            #print(rarity)
 
     def get_processed_data(self):
         return self.data
